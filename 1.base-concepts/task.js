d955c3cc3ec98765e3fa6abf65712e523d23cc46
Annie Bauer <overheads165@gmail.com>
2021-10-26 20:05:53 +0200

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
+'use strict'
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    return [];
+  }
+
+  if (d === 0) {
+    return [-b / (2 * a)];
+  }
+
+  if (d > 0) {
+    return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
