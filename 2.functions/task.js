c9320266d6c4664e303808fdd30af075d84806a7
Annie Bauer <overheads165@gmail.com>
2021-11-03 22:30:05 +0100

    -> functional

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,41 +1,16 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min = arr[0];
-  let max = arr[0];
-  let sum = 0;
-  let avg;
-
-  for (i = 0; i < arr.length; i++) {
-    if (min > arr[i]) {
-      min = arr[i];
-    }
-  }
-
-  for (i = 0; i < arr.length; i++) {
-    if (max < arr[i]) {
-      max = arr[i];
-    }
-  }
-
-  for (i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
-  }
-
-  avg = Math.round((sum / arr.length) * 100) / 100;
+  const min = arr.reduce((prev, x) => prev < x ? prev : x, arr[0])
+  const max = arr.reduce((prev, x) => prev > x ? prev : x, arr[0])
+  const sum = arr.reduce((prev, x) => prev + x, 0)
+  const avg = Math.round((sum / arr.length) * 100) / 100;
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
-function worker(arr) {
-  let sum = 0;
+const worker = (arr) => arr.reduce((prev, x) => prev + x, 0)
 
-  for (i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
-  }
-
-  return sum;
-}
 
 function makeWork(arrOfArr, func) {
   let max = func(arrOfArr[0]);
@@ -52,20 +27,8 @@ function makeWork(arrOfArr, func) {
 
 // Задание 3
 function worker2(arr) {
-  let min = arr[0];
-  let max = arr[0];
-
-  for (i = 0; i < arr.length; i++) {
-    if (max < arr[i]) {
-      max = arr[i];
-    }
-  }
-
-  for (i = 0; i < arr.length; i++) {
-    if (min > arr[i]) {
-      min = arr[i];
-    }
-  }
+  const min = arr.reduce((prev, x) => prev < x ? prev : x, arr[0])
+  const max = arr.reduce((prev, x) => prev > x ? prev : x, arr[0])
 
   return max-min
 }
