cac195d7f3c08ed2dcd6f51a1bf1ddaa9d65d74d
Annie Bauer <overheads165@gmail.com>
2021-11-08 21:10:19 +0100

    pam

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -78,22 +78,83 @@ class Library {
   findBookBy(type, value) {
     switch (type) {
       case "type":
-        break;
+        let bookByType = this.books.find((book) => book.type === value);
+        if (typeof bookByType === "undefined") {
+          return null;
+        } else {
+          return bookByType;
+        }
       case "author":
-        break;
+        let bookByAuthor = this.books.find((book) => book.author === value);
+        if (typeof bookByAuthor === "undefined") {
+          return null;
+        } else {
+          return bookByAuthor;
+        }
       case "name":
-        break;
+        let bookByName = this.books.find((book) => book.name === value);
+        if (typeof bookByName === "undefined") {
+          return null;
+        } else {
+          return bookByName;
+        }
       case "releaseDate":
-          let result = this.books.find((book) => book.releaseDate === value);
-          if (typeof result !== "undefined") {
-            return result
-          }
-          return null
-
+        let bookByRelease = this.books.find(
+          (book) => book.releaseDate === value
+        );
+        if (typeof bookByRelease === "undefined") {
+          return null;
+        } else {
+          return bookByRelease;
+        }
       default:
         return null;
+    }
+  }
+
+  giveBookByName(bookName) {
+    let book = this.books.find((book) => book.name === bookName);
+
+    if (typeof book === "undefined") {
+      return null;
+    } else {
+      let index = this.books.indexOf(book);
+      if (index > -1) {
+        this.books.splice(index, 1);
+      }
+      return book;
     }
   }
 }
 
 //Проверка
+let myLibrary = new Library("Библиотека имени меня");
+
+let book1 = new Book("Тим Феррисс", "4х-часовая рабочая неделя", 2000, 600);
+let book2 = new NovelBook("Джонатан Сафран Фоер", "Планета", 2021, 340);
+let book3 = new NovelBook(
+  "Джеймс Оливер Кервуд",
+  "Бродяги с севера",
+  1919,
+  400
+);
+
+myLibrary.addBook(book1);
+myLibrary.addBook(book2);
+myLibrary.addBook(book3);
+
+console.log(myLibrary.findBookBy("releaseDate", 1919));
+
+console.log("Количество книг до выдачи: " + myLibrary.books.length); 
+let givenBook = myLibrary.giveBookByName("Планета")
+console.log("Количество книг после выдачи: " + myLibrary.books.length); 
+
+console.log(givenBook.state)
+givenBook.state = 30
+console.log(givenBook.state)
+
+givenBook.fix()
+console.log(givenBook.state)
+
+myLibrary.addBook(givenBook)
+console.log("Количество книг после добавления исправленной книги: " + myLibrary.books.length);
